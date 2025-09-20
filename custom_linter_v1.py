6d23ebe5cd1e67f054b38308e8dd99af4a85210b
PC <aluisaybar@gmail.com>
2025-09-20 09:45:51 -0500

    first test v_2_1

--- custom_linter_v1.py
+++ custom_linter_v1.py
@@ -3,41 +3,46 @@ import argparse
 import re
 import sys
 
-def check_blank_lines_between_blocks(file_path):
-    errors = 0
 
+
+def check_black (file_path):
+    errors = 0
+    
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
         for i in range(2, len(lines)):
-            
-            if re.match(r'^\s*(class|def|if|for|while|with)', lines[i]):
-                
-                if not re.match(r'^\s*$', lines[i - 1]) or not re.match(r'^\s*$', lines[i - 2]):
-                    print(f"{file_path}:{i+1}: Expected exactly 2 blank lines before block definition")
+
+            if re.match(r'^\s*(class|def|if|for|while)', lines[i]):
+
+                if not re.match(r'^\s*', lines[i-1]) or not re.match(r'^\s*', lines[i-2]):
+
+                    print(f"{file_path}:{i+1}: Expected 2 black line befone block definition")
                     errors += 1
 
-                elif re.match(r'^\s*$', lines[i - 3]):
-                    print(f"{file_path}:{i+1}: Too many blank lines before block definition (more than 2)")
+                elif re.match(r'^\s*', lines[i-3]):
+                    print(f"{file_path}:{i+1}: Too many line befone block definition")
                     errors += 1
 
     return errors
 
 
+
 def lint_directory(directory):
-    total_errors = 0
+    total_error = 0
 
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith('.py'):
+            if file.endswitch('.py'):
                 file_path = os.path.join(root, file)
-                total_errors += check_blank_lines_between_blocks(file_path)
+                total_error += check_black(file_path)
 
-    return total_errors
+    return total_error
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Custom Linter")
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser(description="Custom linter")
     parser.add_argument("directories", nargs='+', help="Directories to lint")
 
     args = parser.parse_args()
@@ -47,6 +52,7 @@ if __name__ == "__main__":
     for directory in args.directories:
         total_errors += lint_directory(directory)
 
+    
     if total_errors > 0:
         sys.exit(1)
 
