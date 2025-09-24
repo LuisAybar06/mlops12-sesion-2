26adf6c647753caf4a19086599887027df68e425
PC <aluisaybar@gmail.com>
2025-09-23 20:36:30 -0500

    first test v_2_8

---
+++ terraform_vertex/main.tf
@@ -0,0 +1,71 @@
+provider "google" {
+    project=var.project_id
+    region=var.region
+}
+
+
+resource "google_bigquery_dataset" "dataset_v1"{
+    dataset_id = var.dataset_id
+    project = var.project
+    location = var.location
+}
+
+
+resource "google_bigquery_table" "table_v1"{
+    table_id = var.table_id
+    dataset_id = google_bigquery_dataset.dataset_v1.dataset_id
+    project = var.project
+
+    schema = jsonencode([
+        {
+            name="age",
+            type="INTEGER"
+            mode="REQUIRED"
+        },
+        {
+            name="workclass",
+            type="STRING"
+            mode="REQUIRED"
+        },
+        {
+            name="occupation",
+            type="STRING"
+            mode="REQUIRED"
+        }
+    ])
+}
+
+
+
+resource "google_bigquery_dataset_iam_binding" "dataset_acces"{
+    dataset_id = var.dataset_id
+    project = var.project
+
+    role = "roles/bigquery.dataViewer"
+    members = [
+        "serviceAccount:mlops-process@mlops12-469915.iam.gserviceaccount.com"
+    ]
+}
+
+
+resource "google_bigquery_routine" "cencus_filter_by_age" {
+  routine_id = var.routine_id
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  project    = var.project_id
+
+  definition_body = <<-SQL
+    BEGIN
+      INSERT INTO `mlops12-469915.github.census_by_age`
+      SELECT age, workclass, occupation
+      FROM `bigquery-public-data.ml_datasets.census_adult_income`
+      WHERE age < input_age;
+    END;
+  SQL
+
+  routine_type = "PROCEDURE"
+
+  arguments {
+    name      = "input_age"
+    data_type     = jsonencode({ "typeKind" : "INT64" })
+  }
+}
---
+++ terraform_vertex/terraform.tfvars
@@ -0,0 +1,6 @@
+project_id="mlops12-469915"
+dataset_id="github"
+table_id="census_by_age"
+region="US"
+routine_id="cencus_filter_by_age"
+
